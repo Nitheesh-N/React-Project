11e2fb182f1903ab04df0a2a3d2420afd0a9437f
nitheesh <nitheesh.n.9052@gmail.com>
2025-10-14 09:34:58 +0530

    second commit TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import ProgressTracker from './components/ProgressTracker.jsx'
-import Taskform from './components/Taskform.jsx'
-import Tasklist from './components/Tasklist.jsx'
+
+import Taskform from './components/Taskform'
+import Tasklist from './components/Tasklist'
+import ProgressTracker from './components/ProgressTracker'
+import { useEffect, useState } from 'react'
+
 export default function App() {
+
+  const [tasks, settasks] = useState([]);
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  })
+  const addTask=(task)=>{
+    settasks([... tasks,task]);
+  }
   return (
     <div>
       <h1>TaskBuddy</h1>
       <p>Your Friendly Task Manager</p>
-      <Taskform />
-      <Tasklist />
+      <Taskform addTask={addTask}/>
+      <Tasklist tasks = {tasks} updateTask = {updateTask} 
+      deleteTask = {deleteTask}/>
       <ProgressTracker />
       <button>Clear all Tasks</button>
     </div>
--- src/components/Taskform.jsx
+++ src/components/Taskform.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
-export default function Taskform() {
+export default function Taskform({addTask}) {
   const [task, setTask]=useState('');
   const [priority, setPriority]=useState('medium');
   const [category, setCategory]=useState('General');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask({text: task, priority, category, completed: false});
+    //reset
+    setTask('');
+    setPriority('medium');
+    setCategory('General');
+  }
+
   return (
-      <form id='task-form'>
-        <div id='inp'>
-          <input type="text" placeholder="Enter the task" 
+      <form onSubmit={handleSubmit} id="task-form">
+        <div id="inp">
+          <input type="text" placeholder="Enter the task" value={task}
           onChange={(e)=> setTask(e.target.value)}/>
           <button type="submit">Add task</button>
-          {/* <h1>{task}</h1> */}
         </div>
       <div id='btns'>
-        <select onChange={(e)=> setPriority(e.target.value)}>
+        <select value={priority} onChange={(e)=> setPriority(e.target.value)}>
           <option value="High">High</option>
           <option value="Medium">Medium</option>
           <option value="Low">Low</option>
         </select>
-        <select onChange={(e)=> setCategory(e.target.value)}>
+        <select value={category} onChange={(e)=> setCategory(e.target.value)}>
           <option value="General">General</option>
           <option value="Work">Work</option>
           <option value="Personal">Personal</option>
